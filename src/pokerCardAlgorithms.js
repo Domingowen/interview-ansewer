529792924833648e481e683135fb607674f4896e
domingowen <domingowen645@gmail.com>
2021-03-16 14:02:25 +0800

    push poker algorithms

--- src/pokerCardAlgorithms.js
+++ src/pokerCardAlgorithms.js
@@ -1,1 +1,9 @@
 
+let Shuffle = () =>
+  [..."A23456789JQK", 10]
+    .reduce((arr, e) => arr.concat([`红心${e}`, `黑桃${e}`, `梅花${e}`, `方块${e}`]), [
+      "大🃏",
+      "小🃏",
+    ])
+    .sort(() => Math.floor(Math.random() * 3 - Math.random() * 3));
+console.log(Shuffle());
